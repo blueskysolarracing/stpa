4710725b1a840c3cfef6ef522be509644274a6cc
aliraeisdanaei <mrrookie2@gmail.com>
2024-11-12 20:51:33 -0500

    enhance diagram classes

--- stpa/diagrams.py
+++ stpa/diagrams.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from collections import Counter
 from dataclasses import dataclass, field
 from operator import attrgetter
@@ -7,14 +7,42 @@ from typing import ClassVar
 
 @dataclass
 class ControlStructure():
-    id: str
-    value: str
+    # id: str
+    name: str
 
+    def __hash__(self):
+        return hash(self.name)
+    
+    def __eq__(self, other) -> bool:
+        if isinstance(other, ControlStructure):
+            return self.name == other.name
+        return False
 
 @dataclass
-class ControlActionFeedback():
-    controller: ControlStructure
+class ActionFeedback(ABC):
     controlled: ControlStructure
+    controller: ControlStructure
 
+@dataclass
+class ControlAction(ActionFeedback):
     action: str
+
+    def __hash__(self):
+        return hash((self.action, self.controlled.name, self.controller.name))
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, ControlAction):
+            return self.action == other.action and self.controlled == other.controlled and self.controller == other.controller
+        return False
+
+@dataclass
+class ControlFeedback(ActionFeedback):
     feedback: str
+
+    def __hash__(self):
+        return hash((self.feedback, self.controlled.name, self.controller.name))
+    
+    def __eq__(self, other) -> bool:
+        if isinstance(other, ControlFeedback):
+            return self.feedback == other.feedback and self.controlled == other.controlled and self.controller == other.controller
+        return False
