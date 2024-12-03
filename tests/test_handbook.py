7afd53b33760c8b4530493ca33c7f7640cdb2b14
aliraeisdanaei <mrrookie2@gmail.com>
2024-12-03 07:35:13 -0500

    add test for losses in handbook

--- tests/test_handbook.py
+++ tests/test_handbook.py
@@ -6,6 +6,18 @@ from definitions import ControlStructure, ControlAction, ControlFeedback
 from definitions import UnsafeControlAction_notProviding
 
 class handbook_example:
+    loss1 = Loss('Loss of life or injury to people')
+    loss2 = Loss('Loss of or damage to vehicle')
+    loss3 = Loss('Loss of or damage to objects outside the vehicle')
+    loss4 = Loss('Loss of mission (e.g. transportation mission, surveillance mission, scientific mission, defense mission, etc.)')
+    loss5 = Loss('Loss of customer satisfaction')
+    loss6 = Loss('Loss of sensitive information')
+    loss7 = Loss('Environmental loss')
+    loss8 = Loss('Loss of power generation')
+    Loss.reset_counter()
+
+    losses = [loss1, loss2, loss3, loss4, loss5, loss6, loss7, loss8]
+
     flightcrew = ControlStructure('Flight Crew')
     aircraft = ControlStructure('Aircraft')
     wbs = ControlStructure('Wheel Braking Subsytem (WBS)')
@@ -23,6 +35,11 @@ class handbook_example:
     diagrams = {flightcrew, aircraft, wbs, othersubsystems, bscu, physicalbrakes, ac1, ac2, ac3, ac4}
 
 example = handbook_example()
+
+def test_losses():
+    for i, loss in enumerate(example.losses, start=1):
+        assert(f'L-{i}' == loss.get_label())
+        assert(f'L-{i}: {loss.description}' == str(loss))
 
 def test_fig2_11():
     xml_filename = '/home/aliraeis/Projekte/BlueSkySolarRacing/stpa/examples/handbook_fig2.11.drawio'
